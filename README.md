69aba619b021e126757be69793dc63ffa269973f
Edward Chen <xchen7@gradcenter.cuny.edu>
2026-04-04 21:47:31 -0400

    con add todo / webmain

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ZenCalendar
+
+# To-do 
+- add task delete function
+- minimal look for task
+- add description to task
+- add date for task
+- add relevant info for task
+
+
+
+# in future
+- adjust the task operation
+- alert using internal theme instead of system's 
--- webmain.py
+++ webmain.py
@@ -1,7 +1,17 @@
 from backend import CalendarAPI, CalManager
+import webview
+import os
 
-if __name__ == "__main__":
+def get_index_path():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(base_dir,"web","total.html")
+def main():
     manager = CalManager()
     manager.load()
     api = CalendarAPI(manager)
-    print(api.create_task("testweb"))
+    index_path = get_index_path()
+    window = webview.create_window(title="ZenCalendar", url=index_path,js_api=api,width=1400,height=900)
+    webview.start()
+
+if __name__ == "__main__":
+    main()
